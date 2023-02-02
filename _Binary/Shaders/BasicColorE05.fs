adf1a07e188dd899329eb7d1136b62cacf1ece9a
Cooper559024 <mcmurray.aidan.c@gmail.com>
2023-02-01 21:53:04 -0500

    finish E03

--- _Binary/Shaders/BasicColorE05.fs
+++ _Binary/Shaders/BasicColorE05.fs
@@ -9,15 +9,13 @@ out vec4 Fragment;
 
 void main()
 {
-	vec3 show = Color;
-	Fragment = vec4(Color,1);
+	Fragment = vec4(Color, 1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
-		show = SolidColor;
+		Fragment = vec4(SolidColor, 1);
 	
 	if(Invert == true)
-		show = vec3(1.0f, 1.0f, 1.0f) - show;
+		Fragment = vec4((vec3(1,1,1) - Color),1);
 
-	Fragment = vec4(show, 1);
 	return;
 }
